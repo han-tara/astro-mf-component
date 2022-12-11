c015b88f543e35876d0e95cc8d02aad923fc0171
han-tara <99055473+han-tara@users.noreply.github.com>
2022-12-11 11:54:32 +0700

    rev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,22 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import federation from '@originjs/vite-plugin-federation';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      federation({
+        name: 'remote_app',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './Card': './src/components/Card.astro'
+        },
+        shared: ['astro']
+      })
+    ]
+  },
   output: 'server',
   adapter: cloudflare(),
   integrations: [tailwind()]
